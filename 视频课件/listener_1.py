4dc9f68bf5d76d3f15ffb522311af290280e5f14
kingname <greensouth@foxmail.com>
2019-04-28 10:09:20 +0800

    更新发布订阅模式视频代码

---
+++ 视频课件/listener_1.py
@@ -0,0 +1,12 @@
+import redis
+import time
+
+client = redis.Redis()
+chat = client.pubsub(ignore_subscribe_messages=True)
+chat.subscribe('闲话1群', '闲话2群')
+while True:
+    message = chat.get_message()
+    if message:
+        print(message['data'].decode())
+    time.sleep(0.5)
+
