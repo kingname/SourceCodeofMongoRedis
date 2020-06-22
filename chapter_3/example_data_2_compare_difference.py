386f4db4b45ffe9f5058bddb324b46ee3a9d1054
janetat <allinhippo@gmail.com>
2020-06-22 11:19:31 +0800

    [chapter_3]完善chapter_3代码

--- chapter_3/example_data_2_compare_difference.py
+++ chapter_3/example_data_2_compare_difference.py
@@ -1,13 +1,23 @@
 from pymongo import MongoClient
 from bson import ObjectId
+
 client = MongoClient()
-database= client.chapter_3
+database = client.chapter_3
 collection = database.example_data_2
+
+# 1. 空值
 # rows = collection.find({'grade': null})
-# rows = collection.find({'grade': None})
+rows = collection.find({'grade': None})
+
+# 2. 布尔值
 # rows = collection.find({'student': True}, {'_id': 0})
+
+# 3. 排序参数
 # rows = collection.find({}, {'_id': 0}).sort('age', -1)
-rows = collection.find({'_id': ObjectId('5b2f75d26b78a61364d09f45')},
-                       {'_id': 0})
+
+# 4. 查询_id
+# rows = collection.find({'_id': ObjectId('5b2f75d26b78a61364d09f45')},
+#                        {'_id': 0})
+
 for row in rows:
     print(row)
